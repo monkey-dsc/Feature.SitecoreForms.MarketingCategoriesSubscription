8122ff0ee753eacd934248683f1b516851528ef3
monkey.dsc <dirk.schaefauer@camao.one>
2020-10-19 15:12:23 +0200

    fixes/Issue#5
    - configured the Sitecore.EmailCampaign.Cd.Services.MarketingPreferencesService to be loaded on a instance configured to "ContentManagement" only

--- src/website/MCS.CM/Configuration/Configurator.cs
+++ src/website/MCS.CM/Configuration/Configurator.cs
@@ -3,6 +3,8 @@ using Feature.SitecoreForms.MarketingCategoriesSubscription.CM.Services;
 using Feature.SitecoreForms.MarketingCategoriesSubscription.Contract.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Sitecore.DependencyInjection;
+using Sitecore.EmailCampaign.Cd.Services;
+using IMarketingPreferencesService = Sitecore.EmailCampaign.Cd.Services.IMarketingPreferencesService;
 
 namespace Feature.SitecoreForms.MarketingCategoriesSubscription.CM.Configuration
 {
@@ -14,6 +16,14 @@ namespace Feature.SitecoreForms.MarketingCategoriesSubscription.CM.Configuration
         {
             serviceCollection.AddTransient<IExmSubscriptionClientApiService, ExmStandaloneSubscriptionClientApiService>();
             serviceCollection.AddTransient<ISubscribeContactService, SubscribeContactService>();
+
+            /*  ToDo:
+                Dirty workaround, the Sitecore.EmailCampaign.Cd.Services.MarketingPreferencesService is configured to
+                be only loaded on role "Standalone or ContentDelivery". If the instance is configured to "ContentManagement" only,
+                the Module will throw a NullReferenceException.
+                https://github.com/monkey-dsc/Feature.SitecoreForms.MarketingCategoriesSubscription/issues Issue #5
+            */
+            serviceCollection.AddTransient<IMarketingPreferencesService, MarketingPreferencesService>();
         }
     }
 }
